d654c03711ab0e57e5890295b77f1d96ff5e25c0
woffles <wolfgang.truong@gmail.com>
2019-03-18 00:50:36 -0700

    finished cl calculator

--- exercises/ruby_challenges/CL_calculator.rb
+++ exercises/ruby_challenges/CL_calculator.rb
@@ -1,26 +1,71 @@
-p 'welcome to the Commandline calculator, this calculator takes in a string of arithmetic and prints out the numeric answer'
-p 'are you ready? yes or no'
+p 'Welcome to the Commandline calculator, this calculator takes '
+p 'in a string of arithmetic and prints out the numeric answer.'
+
+p 'Are you ready? yes or no'
 input = 'no'
 until input === "yes"
   input = gets.chomp
 end
-`clear`
-
-#((2+5)+20-5*3)
-# regex match (digit +-*/ * digit *)
-# if match solve substring
-# and replace repeat until no more matches.
-# solve helper
+satisfied = 'no'
 def solve(string)
-  # from left to right * or / then + or -
-end
-def seperate(string)
-  #regex for order of operation
-  while regex match is true
-    #replace substring matched with result of solve(substring)
-  return string
-end
-def calculate(string)
-  seperate(string)
+  i = 0
+  string = string.scan(/[\d]+\.?0?|[\D]/)
+  while i < string.length
+    if string[i] == "/"
+      sub = string[i-1].to_f/string[i+1].to_f
+      string[i] = sub
+      string.delete_at(i-1)
+      string.delete_at(i) 
+      i = 0
+    elsif  string[i] == "*"
+      sub = string[i-1].to_f*string[i+1].to_f
+      string[i] = sub
+      string.delete_at(i-1)
+      string.delete_at(i) 
+      i = 0
+    else 
+      i += 1
+    end
+  end
+    i = 0
+    while i < string.length
+    if string[i] == "-"
+      sub = string[i-1].to_f-string[i+1].to_f
+      string[i] = sub
+      string.delete_at(i-1)
+      string.delete_at(i) 
+      i = 0
+    elsif  string[i] == "+"
+      sub = string[i-1].to_f+string[i+1].to_f
+      string[i] = sub
+      string.delete_at(i-1)
+      string.delete_at(i) 
+      i = 0
+    else 
+      i += 1
+    end
+  end
+  return string.join('')
 end
 
+def seperate(string)
+  return string.scan(/\(\d+\D?\d+?\)/)[0]
+end
+
+def calculate(string)
+  og_sub = seperate(string)
+  while og_sub 
+    substring = solve(og_sub.delete('()'))
+    string.sub!(og_sub, substring)
+    p string
+    og_sub = seperate(string)
+  end
+  return solve(string)
+end
+until satisfied === "yes"
+  p "please enter your expression"
+  input = gets.chomp
+  p "Answer: " + calculate(input).to_s
+  p 'Are you satisfied? yes or no?'
+  satisfied = gets.chomp 
+end
